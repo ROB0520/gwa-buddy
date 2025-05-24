540aa6b7d98032aff4be01bcce03ed3c1807ff0f
ROB0520 <ynoreyesminecraft@gmail.com>
2025-05-24 23:35:01 +0800

    Add NEUST Portal Directory card with link and description
    Introduces a new card to the homepage highlighting the NEUST Portal Directory.
    The card includes a title, description, and a button linking to an external website,
    providing users with a resource to check the status of department portals.
    Improves functionality and enhances user experience by offering helpful information.

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@
 
 import GwaCalculator from "@/components/gwa-calculator";
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-svh flex flex-col gap-5 items-center justify-center p-5 overflow-auto">
-      <div className="flex flex-row gap-3 md:gap-5 items-center">
+    <div className="flex flex-col items-center justify-center gap-5 p-5 overflow-auto min-h-svh">
+      <div className="flex flex-row items-center gap-3 md:gap-5">
         <Logo className="h-12 md:h-24 fill-foreground" />
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
           GWA Buddy
         </h1>
       </div>
-      <p className="leading-7 text-center max-w-3xl">
+      <p className="max-w-3xl leading-7 text-center">
         A web application that helps NEUST students calculate their General Weighted Average (GWA) based on their grades.
       </p>
       <Card className="w-full max-w-3xl">
@@ -29,6 +31,23 @@ export default function Home() {
         </CardFooter>
       </Card>
       <GwaCalculator />
+      <Card className="w-full max-w-3xl">
+        <CardHeader>
+          <CardTitle>NEUST Portal Directory</CardTitle>
+          <CardDescription>Stay informed about your department&apos;s portal status</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Wondering if your department&apos;s portal is online or if it&apos;s just a slow connection? Check out the <a href="https://neust-portal.link" className="underline text-primary underline-offset-2 hover:text-primary/80">NEUST Portal Directory</a> page I made to view the current status of the portals.</p>
+          <p></p>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+            <Button asChild>
+              <Link href="https://neust-portal.link">
+                Visit Website
+              </Link>
+            </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
