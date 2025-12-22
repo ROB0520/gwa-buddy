10776e41332bb6edadb51cb0afe8c157b220cd62
ROB0520 <ynoreyesminecraft@gmail.com>
2025-12-22 19:28:12 +0800

    Adds courses for year 1, semester 1 in the latest curriculum of BSHM

--- data/curriculums/bshm/latest.ts
+++ data/curriculums/bshm/latest.ts
@@ -5,6 +5,47 @@ export default {
 	internalName: "latest",
 	term: [
 		{
+			year: 1,
+			semester: 1,
+			courses: [
+				{
+					code: "PATHFit 01",
+					name: "Physical Activity Towards Health and Fitness 1: Movement Competency Training",
+					units: 2
+				},
+				{
+					code: "ELP",
+					name: "English for Hospitality Industry",
+					units: 3
+				},
+				{
+					code: "GE 1",
+					name: "Understanding the Self",
+					units: 3
+				},
+				{
+					code: "GE Fil 1",
+					name: "Kontekswalisadong Komunikasyon sa Filipino",
+					units: 3
+				},
+				{
+					code: "THC 1",
+					name: "Philippine Culture and Tourism Geography",
+					units: 3
+				},
+				{
+					code: "THC 2",
+					name: "Risk Management as Applied to Safety, Security and Sanitation",
+					units: 3
+				},
+				{
+					code: "THC 3",
+					name: "Professional Development and Applied Ethics",
+					units: 3
+				},
+			]
+		},
+		{
 			year: 2,
 			semester: 2,
 			courses: [
